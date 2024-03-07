e77c93956eccc88068e8f95299f4a975ef134be8
sejal.s <ssi@narola.email>
2024-03-07 11:49:33 +0530

    Route change

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -3,7 +3,7 @@ import { Route, Routes } from 'react-router-dom';
 
 const Layout = React.lazy(() => import("../layout"));
 const Products = React.lazy(() => import("../page/products/Products"));
-const AddProduct = React.lazy(() => import("../page/products/AddProduct"));
+const AddUpdateProduct = React.lazy(() => import("../page/products/AddUpdateProduct"));
 const ProductDetail = React.lazy(() => import("../page/products/ProductDetail"));
 const PageNotFound = React.lazy(() => import("../page/PageNotFound"));
 
@@ -12,7 +12,8 @@ function Router() {
     <Routes>
     <Route element={<Layout />}>
       <Route exact path="/" element={<Products />} />
-      <Route exact path="/add-product" element={<AddProduct />} />
+      <Route exact path="/add-product" element={<AddUpdateProduct />} />
+      <Route exact path="/edit-product/:productId" element={<AddUpdateProduct />} />
       <Route exact path="/product-detail/:productId" element={<ProductDetail />} />
       <Route path="*" element={<PageNotFound />} />
     </Route>
